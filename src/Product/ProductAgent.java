6c2b3d1e6c5f46006dac35204b16701a257ace48
danir1512 <61586954+danir1512@users.noreply.github.com>
2022-05-02 15:18:11 +0100

    Alterei cenas

--- src/Product/ProductAgent.java
+++ src/Product/ProductAgent.java
@@ -8,11 +8,12 @@ import jade.core.behaviours.SequentialBehaviour;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
+import jade.proto.AchieveREInitiator;
 import jade.proto.ContractNetInitiator;
-import jade.proto.ContractNetResponder;
 import jade.util.Logger;
 
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.Vector;
 import java.util.logging.Level;
 
@@ -28,8 +29,7 @@ public class ProductAgent extends Agent {
     String current_pos, next_pos;
     boolean recovery_tried, quality_check;
     AID bestResource, agv, ta; //AID -> agent identifier
-    boolean request_agv;
-    public boolean search_InDF_done = false;
+    boolean request_agv, ra_negotiation_done, transport_done;
 
     @Override
     protected void setup() {
@@ -66,52 +66,89 @@ public class ProductAgent extends Agent {
     }
 
     private ArrayList<String> getExecutionList(String productType) {
-        switch (productType) {
-            case "A":
-                return Utilities.Constants.PROD_A;
-            case "B":
-                return Utilities.Constants.PROD_B;
-            case "C":
-                return Utilities.Constants.PROD_C;
-        }
-        return null;
+        return switch (productType) {
+            case "A" -> Utilities.Constants.PROD_A;
+            case "B" -> Utilities.Constants.PROD_B;
+            case "C" -> Utilities.Constants.PROD_C;
+            default -> null;
+        };
     }
 
-    /*
-        behaviour - searchResourceInDF:
-            - Encarregado de procurar os recursos no DF. Apenas é executado uma vez (OneShotBehaviour)
-        methods:
-            - public searchResourceInDF -- constructor
-            - public void action  -------- ???
+    /**
+     behaviour - initiator ContractNetInitiator:
+     - Encarregado de começar as negociações com os RA
+     - public initiator -- constructor
+     - protected handleInform -- This method is called every time a inform message is received
+     - protected handleAllResponses -- This method is called when all the responses have been collected or when the timeout is expired, meaning,
+     it collects all agents with a proposal an accept the best one.
      */
 
     private class initiator extends ContractNetInitiator {
 
+        private final ACLMessage msg;
+
         public initiator(Agent a, ACLMessage msg) {
+
             super(a, msg);
+            this.msg = msg;
         }
 
         @Override
         protected void handleInform(ACLMessage inform) {
-            System.out.println(myAgent.getLocalName() + ": INFORM message received");
+            System.out.println(myAgent.getLocalName() + ": INFORM message received. Next Location: "+ inform.getContent());
+            next_pos = inform.getContent();
+            request_agv = !current_pos.equals(next_pos);
+            ra_negotiation_done = true;
         }
 
         @Override
         protected void handleAllResponses(Vector responses, Vector acceptances) {
-            System.out.println(myAgent.getLocalName() + ": ALL PROPOSALS received");
-            ACLMessage auxMsg = (ACLMessage) responses.get(0);
-            ACLMessage reply = auxMsg.createReply();
-            reply.setPerformative(ACLMessage.ACCEPT_PROPOSAL);
-            acceptances.add(reply);
+            System.out.println(myAgent.getLocalName() + ": All PROPOSALS received");
+
+            //Evaluate proposals
+            int bestProposal = -1;
+            AID bestProposer = null;
+            ACLMessage accept = null;
+            Enumeration e = responses.elements();
+
+            while(e.hasMoreElements()){
+
+                ACLMessage msg = (ACLMessage)e.nextElement();
+
+                if(msg.getPerformative() == ACLMessage.PROPOSE){
+                    ACLMessage reply = msg.createReply();
+                    reply.setPerformative(ACLMessage.REJECT_PROPOSAL);
+                    acceptances.addElement(reply);
+
+                    int proposal = Integer.parseInt(msg.getContent()); // to define in RA (can be a random)
+                    if(proposal > bestProposal){
+                        bestProposal = proposal;
+                        bestProposer = msg.getSender();
+                        accept = reply;
+                    }
+                }
+                else{
+                    System.out.println("(CFP) REFUSE received from: " + msg.getSender().getLocalName());
+                }
+                //Acept best proposal
+                if(accept != null){
+                    System.out.println("Accepting proposal " + bestProposal + " from responder " + bestProposer.getLocalName());
+                    accept.setPerformative(ACLMessage.ACCEPT_PROPOSAL);
+
+                    bestResource = bestProposer;
+                }
+
+                myAgent.addBehaviour(new initiator(myAgent,this.msg));
+            }
         }
     }
 
-    /*
-        behaviour - searchResourceInDF:
-            - Encarregado de procurar os recursos no DF. Apenas é executado uma vez (OneShotBehaviour)
-        methods:
-            - public searchResourceInDF -- constructor
-            - public void action  -------- ???
+    /**
+     behaviour - searchResourceInDF:
+     - Encarregado de procurar os recursos no DF. Apenas é executado uma vez (OneShotBehaviour)
+     methods:
+     - public searchResourceInDF -- constructor
+     - public void action  -------- ???
      */
 
     private class searchResourceInDF extends OneShotBehaviour {
@@ -133,17 +170,55 @@ public class ProductAgent extends Agent {
             }
 
             if (agents_list != null) {
-                ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+                ACLMessage msg = new ACLMessage(ACLMessage.CFP);
                 for (int i = 0; i < agents_list.length; i++) {
                     msg.addReceiver(agents_list[i].getName());
                     System.out.println("Msg  sent to:" + agents_list[i].getName().getLocalName());
                 }
+                myAgent.addBehaviour(new initiator(myAgent,msg));
             } else {
-                System.out.println("RIP");
+                System.out.println("RIP - Resource not found: " + executionPlan.get(plan_step));
             }
 
 
         }
     }
 
+    /**
+     *
+     * behaviour - comStart
+     * - Inicia as comunicação entre os agentes
+     * - protected void handleAgree
+     * - protected handleInform
+     * - protected handleRefuse
+     *
+     */
+
+    private class comStart extends AchieveREInitiator {
+
+        private final ACLMessage msg;
+
+        public comStart(Agent a, ACLMessage msg) {
+            super(a, msg);
+            this.msg = msg;
+        }
+
+        @Override
+        protected void handleAgree(ACLMessage agree) {
+            super.handleAgree(agree);
+        }
+
+        @Override
+        protected void handleInform(ACLMessage inform) {
+            System.out.println(myAgent.getLocalName() + ": INFORM message received");
+
+            current_pos = next_pos;
+            transport_done = true;
+        }
+
+        @Override
+        protected void handleRefuse(ACLMessage refuse) {
+
+        }
+    }
 }
