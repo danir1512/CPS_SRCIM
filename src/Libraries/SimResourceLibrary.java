09aa1635c8706635e8f6cb3f1104414887d83278
João <jpal.oliveira@campus.fct.unl.pt>
2022-05-09 15:17:29 +0100

    Quase

--- src/Libraries/SimResourceLibrary.java
+++ src/Libraries/SimResourceLibrary.java
@@ -34,7 +34,7 @@ public class SimResourceLibrary implements IResource {
             case "GlueStation2": port=19998; break;
             case "QualityControlStation1": port=19999; break;
             case "QualityControlStation2": port=20000; break;
-            case "Operator": port=20001; break;
+            case "Source": port=20001; break;
         }
         clientID = sim.simxStart("127.0.0.1", port, true, true, 5000, 5);        
         if (clientID != -1) {
@@ -84,7 +84,7 @@ public class SimResourceLibrary implements IResource {
                 skills = new String[1];
                 skills[0] = Utilities.Constants.SK_QUALITY_CHECK;
                 return skills;
-            case "Operator":
+            case "Source":
                 skills = new String[2];
                 skills[0] = Utilities.Constants.SK_PICK_UP;
                 skills[1] = Utilities.Constants.SK_DROP;
